2832d84ff65076f22565ec75463de0377964e758
DorianGrey2012 <mechtatelnyi1@mail.ru>
2019-01-03 16:05:31 +0400

    Update transmitter.ino

--- transmitter.ino
+++ transmitter.ino
@@ -1,150 +1,155 @@
-#include <Ethernet.h>
+/**
+ * 
+ * Transmitter for fishing-bait system.
+ *
+ * Using LowPower library to put Arduino in sleep mode.
+ * See: https://github.com/rocketscream/Low-Power
+ *
+ * Using nRF24L01 library for RF interface.
+ * See: https://github.com/aaronds/arduino-nrf24l01
+ *
+ * Pins:
+ * Hardware SPI:
+ * MISO -> 12
+ * MOSI -> 11
+ * SCK -> 13
+ *
+ * Configurable:
+ * CE -> 9
+ * CSN -> 8
+ */
+
 #include <SPI.h>
-#include <Arduino.h>
-#include "WebServer.h"
+#include <Mirf.h>
+#include <nRF24L01.h>
+#include <MirfHardwareSpiDriver.h>
 
-unsigned long valueold;
+#include <LowPower.h>
 
+volatile bool do_transmission;
+byte buttonNr;
 
-char buf[60]; //буфер для отправки данных. 
+void setup(){
+  // Setup button nr
+  setButtonNr();
+  char receiverAddr[] = "button0";
+  receiverAddr[6] = '1' + buttonNr;
 
-byte ip[] = { 192, 168, 1, 10 }; // локальный адрес
-byte rserver[] = { 192, 168, 1, 20 };  // ip-адрес удалённого сервера
+  // Setup MIRF board
+  Mirf.cePin = 8;
+  Mirf.csnPin = 9;
 
+  Mirf.spi = &MirfHardwareSpi;
+  Mirf.init();
 
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0x54, 0xDE, 0x33 }; // MAC-адрес нашего устройства
-byte subnet[] = { 255, 255, 255, 0 };
-byte gateway[] = { 192, 168, 1,1 };
-byte dns_server[] = { 192, 168, 1, 1};
+  Mirf.setRADDR((byte *) receiverAddr);
 
-#define VERSION_STRING "Radio server"
+  Mirf.payload = sizeof(byte);
 
-P(Page_info) = "<html><head><title>controller " VERSION_STRING "</title></head><body>\n";
-P(location_info) = "server room";
-P(version_info) = VERSION_STRING ". Compile date: " __DATE__ " " __TIME__;
+  // we use channel 90 as it is outside of WLAN bands
+  // or channels used by wireless surveillance cameras
+  Mirf.channel = 90;
 
-String url = String(25);
+  Mirf.config();
 
+  // Setup button
+  pinMode(2, INPUT);           // set pin to input
+  digitalWrite(2, HIGH);       // turn on pullup resistors
 
-#define MAX_COMMAND_LEN             (10)
-#define MAX_PARAMETER_LEN           (10)
-#define COMMAND_TABLE_SIZE          (8)
-#define PREFIX ""
+  // Setup other variables
+  do_transmission = false;
 
-WebServer webserver(PREFIX, 80);
-EthernetClient client;
+  // Reduce power
+  pinsLow();
+}
 
-
-#define NAMELEN 32
-#define VALUELEN 32
-
-char gCommandBuffer[MAX_COMMAND_LEN + 1];
-char gParamBuffer[MAX_PARAMETER_LEN + 1];
-long gParamValue;
-
-typedef struct {
-  char const    *name;
-  void          (*function)(WebServer &server);
-} command_t;
-
-
-void infoRequest(WebServer &server, WebServer::ConnectionType type, char *url_tail, bool tail_complete)
+void loop()
 {
-  server.printP(Page_info);
-  server.print("IP:");
-  server.print(Ethernet.localIP());
-  server.print("<br>Location:");
-  server.printP(location_info);
+  if (do_transmission == true)
+  {
+    // Clear flag
+    do_transmission = false;
+
+    // Setup packet data
+    Mirf.setTADDR((byte *) "receiver");
+
+    // Send data
+    Mirf.send((byte *) & buttonNr);
+
+    // Wait for sending to finish
+    while (Mirf.isSending())
+    {
+    }
+
+    // Power down the MIRF module
+    Mirf.powerDown();
+
+
+    delay(50);
+  } else
+  {
+    // Wait for pin 2 to go high
+    while (!digitalRead(2))
+    {
+      delay(50);
+    }
+
+    // Go to sleep
+    sleepNow();
+  }
 }
 
-void setup() {
-  Ethernet.begin(mac, ip, dns_server, gateway, subnet); // Инициализируем Ethernet Shield
+void wakeUpNow ()
+{
+  // Don't put detach here, seems to lock up chip,
+  // i.e. on button bounce while trying to go into sleep.
+  // Detach the interrupt so it doesn't fire again
+  //detachInterrupt(0);
 
-  webserver.setDefaultCommand(&infoRequest); // дефолтная страница
-  webserver.begin();
-  mySwitch.enableReceive(0); // включаем прием на 2 выводе (прерывание 0)
+  // Set flag
+  do_transmission = true;
 }
 
-bool ledA=0,ledB=0,ledC=0,ledD=0;
-long statesend = 0; 
+void setButtonNr ()
+{
 
-void loop() {
-  
-  char buff[64];
-  int len = 64;
-  webserver.processConnection(buff, &len);  // process incoming connections one at a time forever
-  
-unsigned long currentMillis = millis(); // текущий счетчик
-
-  if (mySwitch.available()) {
-unsigned long valuesend = mySwitch.getReceivedValue();
-
-#if 0 // прием команд с радиопультов, выключено ! При использовании не забудьте про установку пина режим на выход..
-if ((unsigned int)valuesend==21952){ // A
-
-if (currentMillis-statesend>1000){
-   statesend=currentMillis;
-    ledA=!ledA;
-    sendstate(5,ledA);
-}
-  
-} else 
-if ((unsigned int)valuesend==21808){ // B
-
-if (currentMillis-statesend>1000){
-   statesend=currentMillis;
-    ledB=!ledB;
-    sendstate(6,ledB);
-}
-  
-} else 
-if ((unsigned int)valuesend==21772){ // C
-
-if (currentMillis-statesend>1000){
-   statesend=currentMillis;
-    ledC=!ledC;
-    sendstate(7,ledC);
-}
-} else 
-if ((unsigned int)valuesend==21763){ // D
-
-if (currentMillis-statesend>1000){
-   statesend=currentMillis;
-    ledD=!ledD;
-    sendstate(8,ledD);
-}
-  
-} else
-#endif
+  buttonNr = 0;
  
- // отправка принятых данных с беспроводных датчиков и отправка их на сервер.
-  if (valuesend != 0 && valueold !=valuesend) 
- {
-   valueold=valuesend;
-        if (client.connect(rserver, 80)) {
-       sprintf (buf,"GET /server.php?mode=rcskey&n=%hu HTTP/1.0",valuesend);
-       
-   client.println(buf);
-       client.println("Host: local.ru");
-    client.println();
-  }  //else Serial.println("connection failed");
-client.stop();
-    
-    
-  }
-  mySwitch.resetAvailable();
-  }
-
 }
 
-void sendstate (int pin,int mode){ // управление нагрузками с пульта с отправкой статуса на сервер
-  digitalWrite(pin,mode);
-  
-              if (client.connect(rserver, 80)) {
-      sprintf(buf, "GET /server.php?mode=key&n=%i&s=%i  HTTP/1.0", pin, mode);
-     client.println(buf);  
-       client.println("Host: local.ru"); 
-    client.println();
-  } 
-    client.stop();
+void pinsLow ()
+{
+  // Make all unused pins low outputs to save power
+  uint8_t const pinNrs[] = {0, 1, 6, 7, 10, A0, A1, A2, A3, A4, A5, A6, A7};
+
+  for (int i = 0; i < sizeof(pinNrs); ++i)
+  {
+    pinMode(pinNrs[i], OUTPUT);
+    digitalWrite(pinNrs[i], LOW);
+  }
+}
+
+void sleepNow ()         // here we put the arduino to sleep
+{
+  // Make sure the nRF24L01+ module is powered down
+  Mirf.powerDown();
+
+  // Make sure we don't go sleeping with pin low!
+  while (!digitalRead(2))
+  {
+  }
+
+ 
+   *
+   * In all but the IDLE sleep modes only LOW can be used.
+   */
+  attachInterrupt(0, wakeUpNow, LOW); // use interrupt 0 (pin 2) and run function
+
+  // Sleep with ADC module and BOD module off
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+
+  // THE PROGRAM CONTINUES FROM HERE AFTER WAKING UP
+
+  // Detach the interrupt so it doesn't fire again
+  detachInterrupt(0);
 }
